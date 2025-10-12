60d22e7f6d8c33e737d1f0db9e27007cf03529f1
kwambokayvonne-11 <kwambokayvonne11@gmail.com>
2025-10-12 11:57:42 +0300

    Add files via upload

---
+++ for_loop.c
@@ -0,0 +1,15 @@
+/*
+Name:Yvonne Kwamboka
+Reg No:PA106/G/28782/25
+Description:program using for loop to display 100-50 in a descending order
+*/
+
+#include<stdio.h>
+int main(){
+int i;
+for(i=100;i>=50;i--)
+{
+    printf("%d\n",i);
+}
+    return 0;
+}
